402ffe2f9232adcda4decc845d625f1cf5496e98
MSadhik <Sadhik.batsha@ibm.com>
2025-08-14 12:43:57 +0530

    Add files via upload

---
+++ SQL/Activities/Activity1.sql
@@ -0,0 +1,5 @@
+--Activity-1: Create saleman table
+
+CREATE TABLE salesman(salesman_id int PRIMARY KEY, salesman_name varchar(32), salesman_city varchar(32), commission int );
+
+DESCRIBE salesman;
